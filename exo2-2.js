524597124856606dcac7a09c7a02be234779eb81
ARO Franco Itobiantsoa <arofr007@gmail.com>
2026-09-18 14:15:58 +0300

    make exo2-2.js

---
+++ exo2-2.js
@@ -0,0 +1,20 @@
+function Ranger(T) {
+  let gauche = 0;
+  let droite = T.length - 1;
+
+  while (gauche < droite) {
+    while (gauche < droite && T[gauche] === 0) {
+      gauche++;
+    }
+
+    while (gauche < droite && T[droite] === 1) {
+      droite--;
+    }
+
+    if (gauche < droite) {
+      Echanger(T, gauche, droite);
+    }
+  }
+
+  return T;
+}
